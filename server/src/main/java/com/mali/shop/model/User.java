852dff15d5ec7111464f186027d1337b0ddc5c03
mszab0310 <mszabi0310@gmail.com>
2022-11-21 11:33:15 +0200

    Implement user model on server and server configuration

---
+++ server/src/main/java/com/mali/shop/model/User.java
@@ -0,0 +1,20 @@
+package com.mali.shop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    private boolean active;
+    private String roles;
+}
