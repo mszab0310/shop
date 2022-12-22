9449b3867073f00b29b3ad30a6a52de76ce7b718
mszab0310 <mszabi0310@gmail.com>
2022-12-22 21:39:12 +0200

    Fix bad query resulting in bean wiring error

--- server/src/main/java/com/mali/shop/repository/ProductRepository.java
+++ server/src/main/java/com/mali/shop/repository/ProductRepository.java
@@ -2,10 +2,12 @@ package com.mali.shop.repository;
 
 import com.mali.shop.model.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    public List<Product> findAll();
-    public List<Product> getProductsByActiveTrue();
+     List<Product> findAll();
+    @Query("SELECT p FROM Product p WHERE p.isActive=true")
+     List<Product> getProductsByActiveTrue();
 }
