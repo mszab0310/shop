1e737ffd7ae30db24c190ddadc8f459d2e1e6304
mszab0310 <mszabi0310@gmail.com>
2022-12-28 13:24:04 +0200

    Some styling for the product page

--- ebay-client/src/pages/product/page/product-specific/ProductPage.tsx
+++ ebay-client/src/pages/product/page/product-specific/ProductPage.tsx
@@ -11,25 +11,42 @@ function ProductPage() {
   const location = useLocation();
   const navigate = useNavigate();
 
-  const [product, setProduct] = useState<Product>();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [imageUrl, setImageUrl] = useState<string>("");
 
   useEffect(() => {
     getProductById(location.state.id)
       .then((res) => {
         setProduct(res.data);
+        setImageUrl("https://source.unsplash.com/random/?" + res.data.name.split(" ")[0]);
       })
       .catch((err) => {
         //probably unauthorized
         navigate(NavigationRoutes.PRODUCTS);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <>
       <Navbar />
-      <CircularProgress />
       <div className="mainContainer">
-        <div>{location.state.id}</div>
+        <div className="loaderContainer">
+          {product === null ? (
+            <CircularProgress sx={{ display: "flex" }} />
+          ) : (
+            <div className="productContainer">
+              <div className="productHeader">
+                <h1> {product.name} </h1>
+                <img src={imageUrl} alt="product" />
+              </div>
+
+              <p> {product.description} </p>
+              {product.isActive}
+              {}
+            </div>
+          )}
+        </div>
       </div>
     </>
   );
